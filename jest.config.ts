d35d33df0ac8394ce36a7baa19c0147868945af5
Luiz Filipe da Silva <filipeluiz.bs@gmail.com>
2026-01-24 16:15:11 -0300

    Update Jest configuration file: now it only includes .spec.ts files inside __testes__ directories

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+// jest.config.ts
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.spec.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+};
+
+export default config;
