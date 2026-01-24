bfb5895c727ef06eb812be60f77e81396b5e3f58
Luiz Filipe da Silva <filipeluiz.bs@gmail.com>
2026-01-24 09:00:03 -0300

    Replace any by explicit convertion

--- src/utils/functions/generic/get-entries.ts
+++ src/utils/functions/generic/get-entries.ts
@@ -1,8 +1,9 @@
 /**
  * Returns the entries of an object.
- * 
+ *
  * @param {object} obj The object to retrieve entries.
- * 
+ *
  * @returns {[string, any]} The entries of the object.
  */
-export const getEntries = <T extends object>(obj: T): [[keyof T, T[keyof T]]] => Object.entries(obj) as any;
+export const getEntries = <T extends object>(obj: T): [[keyof T, T[keyof T]]] =>
+  Object.entries(obj) as [[keyof T, T[keyof T]]];
