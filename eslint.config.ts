2efceb3fb4dec5bd6786fd4ff08c70c3d5fdd4c1
Luiz Filipe da Silva <filipeluiz.bs@gmail.com>
2026-03-17 20:46:35 -0300

    Replace tslint by eslint

---
+++ eslint.config.ts
@@ -0,0 +1,45 @@
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+  {
+    ignores: ['./node_modules/**', '**/__tests__/**', 'eslint.config.ts'],
+  },
+
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'module',
+      parserOptions: {
+        project: './tsconfig.eslint.json',
+        tsconfigRootDir: '.',
+      },
+    },
+  },
+
+  {
+    rules: {
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      'arrow-body-style': ['error', 'as-needed'],
+    },
+  },
+]);
